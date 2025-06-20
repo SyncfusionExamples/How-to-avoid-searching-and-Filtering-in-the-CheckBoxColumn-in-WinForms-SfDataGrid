9d9059ad613c3c81096af1f57786dfc7998a362e
SweathaBharathi <sweatha.bharathi@syncfusion.com>
2025-06-20 15:25:57 +0530

    To avoid searching and Filtering in the CheckBoxColumn in WinForms SfDataGrid

---
+++ GridCheckBoxColumnFiltering/Model/OrderInfo.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGrid_Demo_4_8
+{
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        private int orderID;
+        private string country;
+        private string customerName;
+        private bool status;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        [Display(Name = "Order ID")]
+        public int OrderID
+        {
+            get => orderID;
+            set
+            {
+                if (orderID != value)
+                {
+                    orderID = value;
+                    OnPropertyChanged(nameof(OrderID));
+                }
+            }
+        }
+
+        [Display(Name = "Status")]
+        public bool Status
+        {
+            get { return status; }
+            set
+            {
+                if (status != value)
+                {
+                    status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        [Display(Name = "Name")]
+        public string CustomerName
+        {
+            get => customerName;
+            set
+            {
+                if (customerName != value)
+                {
+                    customerName = value;
+                    OnPropertyChanged(nameof(CustomerName));
+                }
+            }
+        }
+
+        [Display(Name = "Country")]
+        public string Country
+        {
+            get => country;
+            set
+            {
+                if (country != value)
+                {
+                    country = value;
+                    OnPropertyChanged(nameof(Country));
+                }
+            }
+        }
+
+        public OrderInfo(int orderId, bool status ,string customerName, string country)
+        {
+            this.OrderID = orderId;
+            this.Status = status;
+            this.CustomerName = customerName;
+            this.Country = country;
+        }
+    }
+
+}
---
+++ GridCheckBoxColumnFiltering/ViewModel/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGrid_Demo_4_8
+{
+    public class OrderInfoCollection
+    {
+        private ObservableCollection<OrderInfo> _orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+        public OrderInfoCollection()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo(1001,true, "Maria", "Germany"));
+            _orders.Add(new OrderInfo(1002, false, "Trujilo", "Mexico"));
+            _orders.Add(new OrderInfo(1003, true, "Moreno", "Mexico"));
+            _orders.Add(new OrderInfo(1004, false, "Hardy", "UK"));
+            _orders.Add(new OrderInfo(1005, true, "Christina", "Sweden"));
+            _orders.Add(new OrderInfo(1006, false, "Hanna", "Germany"));
+            _orders.Add(new OrderInfo(1007, true, "Frederique", "France"));
+            _orders.Add(new OrderInfo(1008, false, "Martin", "Spain"));
+            _orders.Add(new OrderInfo(1009, true, "Laurence", "France"));
+            _orders.Add(new OrderInfo(1010, false, "Elizabeth", "Canada"));
+        }
+    }
+}
